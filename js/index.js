36ee940b3a4b7da4583af096cbd055854212ccb6
Eric Skogen <audionerd@users.noreply.github.com>
2020-08-04 14:38:47 -0500

    organize javascripts

--- js/index.js
+++ js/index.js
@@ -1,123 +1,13 @@
-function initializeSidenotes() {
-  let sidebar = document.querySelector('.right-sidebar')
-  let asides = [...document.querySelectorAll('aside[name]')]
-  for (let aside of asides) {
-    aside.parentNode.removeChild(aside)
-    sidebar.appendChild(aside)
-  }
-}
-function updateSidenotes() {
-  let asides = [...document.querySelectorAll('aside[name]')]
-
-  for (let aside of asides) {
-    let name = aside.getAttribute('name')
-    let span = document.querySelector(`span[name="${name}"]`)
-    // let containerRect = document.querySelector('.right-sidebar').getBoundingClientRect();
-    let containerRect = document.body.getBoundingClientRect()
-    let elemRect = span.getBoundingClientRect()
-
-    let styles = window.getComputedStyle(span)
-    let spanLineHeight = parseInt(styles.getPropertyValue('line-height')) * 0.7 // optical adjustment
-
-    let offset = 0
-    offset += span.offsetParent.offsetTop
-
-    aside.style.top =
-      Math.ceil(elemRect.bottom - spanLineHeight - containerRect.top - offset) +
-      'px'
-    aside.style.display = 'block'
-  }
-}
+import sidenotes from './sidenotes.js'
+import galleries from './galleries.js'
 
 function resize() {
-  updateSidenotes()
+  sidenotes.update()
 }
 
-var parseThumbnailElements = function (el) {
-  var thumbElements = el.childNodes,
-    numNodes = thumbElements.length,
-    items = [],
-    figureEl,
-    linkEl,
-    size,
-    item
-
-  for (var i = 0; i < numNodes; i++) {
-    figureEl = thumbElements[i] // <figure> element
-
-    // include only element nodes
-    if (figureEl.nodeType !== 1) {
-      continue
-    }
-
-    linkEl = figureEl.children[0] // <a> element
-
-    size = linkEl.getAttribute('data-size').split('x')
-
-    // create slide object
-    item = {
-      src: linkEl.getAttribute('href'),
-      w: parseInt(size[0], 10),
-      h: parseInt(size[1], 10)
-    }
-
-    if (figureEl.children.length > 1) {
-      // <figcaption> content
-      item.title = figureEl.children[1].innerHTML
-    }
-
-    if (linkEl.children.length > 0) {
-      // <img> thumbnail element, retrieving thumbnail url
-      item.msrc = linkEl.children[0].getAttribute('src')
-    }
-
-    item.el = figureEl // save link to element for getThumbBoundsFn
-    items.push(item)
-  }
-
-  return items
-}
-
-;(() => {
-  let pswpElement = document.querySelectorAll('.pswp')[0]
-
-  let options = {
-    mainClass: 'pswp--minimal--dark',
-    barsSize: { top: 0, bottom: 0 },
-    captionEl: false,
-    fullscreenEl: false,
-    shareEl: false,
-    bgOpacity: 0.85,
-    tapToClose: true,
-    tapToToggleControls: false,
-    history: false
-  }
-
-  let click = (event) => {
-    event.preventDefault()
-
-    if (event.target.tagName === 'IMG') {
-      let gallery = event.target.closest('[data-gallery]')
-      let parent = event.target.closest('a')
-      let index = [
-        ...event.target.closest('[data-gallery]').querySelectorAll('a')
-      ].indexOf(parent)
-      let items = parseThumbnailElements(gallery)
-
-      let ps = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, {
-        ...options,
-        index
-      })
-      ps.init()
-    }
-  }
-  let galleries = [...document.querySelectorAll('[data-gallery]')]
-  for (let gallery of galleries) {
-    gallery.addEventListener('click', click)
-  }
-})()
-
-initializeSidenotes()
 window.addEventListener('resize', resize)
+
+galleries.init()
+sidenotes.init()
 resize()
 setTimeout(resize, 200)
---
+++ js/sidenotes.js
@@ -0,0 +1,35 @@
+function init() {
+  let sidebar = document.querySelector('.right-sidebar')
+  let asides = [...document.querySelectorAll('aside[name]')]
+  for (let aside of asides) {
+    aside.parentNode.removeChild(aside)
+    sidebar.appendChild(aside)
+  }
+}
+function update() {
+  let asides = [...document.querySelectorAll('aside[name]')]
+
+  for (let aside of asides) {
+    let name = aside.getAttribute('name')
+    let span = document.querySelector(`span[name="${name}"]`)
+    // let containerRect = document.querySelector('.right-sidebar').getBoundingClientRect();
+    let containerRect = document.body.getBoundingClientRect()
+    let elemRect = span.getBoundingClientRect()
+
+    let styles = window.getComputedStyle(span)
+    let spanLineHeight = parseInt(styles.getPropertyValue('line-height')) * 0.7 // optical adjustment
+
+    let offset = 0
+    offset += span.offsetParent.offsetTop
+
+    aside.style.top =
+      Math.ceil(elemRect.bottom - spanLineHeight - containerRect.top - offset) +
+      'px'
+    aside.style.display = 'block'
+  }
+}
+
+export default {
+  init,
+  update
+}
