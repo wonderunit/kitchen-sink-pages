bf3a2eaa028dc80e0182d745ad976a3b6a714a4c
Eric Skogen <audionerd@users.noreply.github.com>
2020-08-04 12:10:20 -0500

    prettier

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
 function initializeSidenotes() {
-  let sidebar = document.querySelector('.right-sidebar');
-  let asides = [...document.querySelectorAll("aside[name]")];
+  let sidebar = document.querySelector('.right-sidebar')
+  let asides = [...document.querySelectorAll('aside[name]')]
   for (let aside of asides) {
     aside.parentNode.removeChild(aside)
     sidebar.appendChild(aside)
   }
 }
 function updateSidenotes() {
-  let asides = [...document.querySelectorAll("aside[name]")];
+  let asides = [...document.querySelectorAll('aside[name]')]
 
   for (let aside of asides) {
-    let name = aside.getAttribute("name");
-    let span = document.querySelector(`span[name="${name}"]`);
+    let name = aside.getAttribute('name')
+    let span = document.querySelector(`span[name="${name}"]`)
     // let containerRect = document.querySelector('.right-sidebar').getBoundingClientRect();
-    let containerRect = document.body.getBoundingClientRect();
-    let elemRect = span.getBoundingClientRect();
+    let containerRect = document.body.getBoundingClientRect()
+    let elemRect = span.getBoundingClientRect()
 
-    let styles = window.getComputedStyle(span);
-    let spanLineHeight = parseInt(styles.getPropertyValue("line-height")) * 0.7; // optical adjustment
+    let styles = window.getComputedStyle(span)
+    let spanLineHeight = parseInt(styles.getPropertyValue('line-height')) * 0.7 // optical adjustment
 
     let offset = 0
-    offset += span.offsetParent.offsetTop;
+    offset += span.offsetParent.offsetTop
 
     aside.style.top =
-      Math.ceil(
-        elemRect.bottom - spanLineHeight - containerRect.top - offset
-      ) + "px";
+      Math.ceil(elemRect.bottom - spanLineHeight - containerRect.top - offset) +
+      'px'
   }
 }
 
 function resize() {
-  updateSidenotes();
+  updateSidenotes()
 }
 
 var parseThumbnailElements = function (el) {
@@ -40,49 +39,49 @@ var parseThumbnailElements = function (el) {
     figureEl,
     linkEl,
     size,
-    item;
+    item
 
   for (var i = 0; i < numNodes; i++) {
-    figureEl = thumbElements[i]; // <figure> element
+    figureEl = thumbElements[i] // <figure> element
 
     // include only element nodes
     if (figureEl.nodeType !== 1) {
-      continue;
+      continue
     }
 
-    linkEl = figureEl.children[0]; // <a> element
+    linkEl = figureEl.children[0] // <a> element
 
-    size = linkEl.getAttribute("data-size").split("x");
+    size = linkEl.getAttribute('data-size').split('x')
 
     // create slide object
     item = {
-      src: linkEl.getAttribute("href"),
+      src: linkEl.getAttribute('href'),
       w: parseInt(size[0], 10),
-      h: parseInt(size[1], 10),
-    };
+      h: parseInt(size[1], 10)
+    }
 
     if (figureEl.children.length > 1) {
       // <figcaption> content
-      item.title = figureEl.children[1].innerHTML;
+      item.title = figureEl.children[1].innerHTML
     }
 
     if (linkEl.children.length > 0) {
       // <img> thumbnail element, retrieving thumbnail url
-      item.msrc = linkEl.children[0].getAttribute("src");
+      item.msrc = linkEl.children[0].getAttribute('src')
     }
 
-    item.el = figureEl; // save link to element for getThumbBoundsFn
-    items.push(item);
+    item.el = figureEl // save link to element for getThumbBoundsFn
+    items.push(item)
   }
 
-  return items;
-};
+  return items
+}
 
-(() => {
-  let pswpElement = document.querySelectorAll(".pswp")[0];
+;(() => {
+  let pswpElement = document.querySelectorAll('.pswp')[0]
 
   let options = {
-    mainClass: "pswp--minimal--dark",
+    mainClass: 'pswp--minimal--dark',
     barsSize: { top: 0, bottom: 0 },
     captionEl: false,
     fullscreenEl: false,
@@ -90,34 +89,34 @@ var parseThumbnailElements = function (el) {
     bgOpacity: 0.85,
     tapToClose: true,
     tapToToggleControls: false,
-    history: false,
-  };
+    history: false
+  }
 
   let click = (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
-    if (event.target.tagName === "IMG") {
-      let gallery = event.target.closest("[data-gallery]");
-      let parent = event.target.closest("a");
+    if (event.target.tagName === 'IMG') {
+      let gallery = event.target.closest('[data-gallery]')
+      let parent = event.target.closest('a')
       let index = [
-        ...event.target.closest("[data-gallery]").querySelectorAll("a"),
-      ].indexOf(parent);
-      let items = parseThumbnailElements(gallery);
+        ...event.target.closest('[data-gallery]').querySelectorAll('a')
+      ].indexOf(parent)
+      let items = parseThumbnailElements(gallery)
 
       let ps = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, {
         ...options,
-        index,
-      });
-      ps.init();
+        index
+      })
+      ps.init()
     }
-  };
-  let galleries = [...document.querySelectorAll("[data-gallery]")];
-  for (let gallery of galleries) {
-    gallery.addEventListener("click", click);
   }
-})();
+  let galleries = [...document.querySelectorAll('[data-gallery]')]
+  for (let gallery of galleries) {
+    gallery.addEventListener('click', click)
+  }
+})()
 
 initializeSidenotes()
-window.addEventListener("resize", resize);
-resize();
-setTimeout(resize, 200);
+window.addEventListener('resize', resize)
+resize()
+setTimeout(resize, 200)
