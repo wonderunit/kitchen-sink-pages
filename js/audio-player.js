b82982be3beef91fa8873fb6fbf329a149f73354
Eric Skogen <audionerd@users.noreply.github.com>
2020-08-17 10:57:56 -0500

    basic audio player

---
+++ js/audio-player.js
@@ -0,0 +1,43 @@
+const init = () => {
+  let player = document.querySelector('audio[data-audio-player]')
+  let ui = document.querySelector('.audio-player')
+
+  let onTimeUpdate = event => {
+    // get the current time
+    let { currentTime } = event.target
+
+    // update the ui
+    let totalTime = event.target.duration
+    ui.querySelector('.progress').style.width = ((currentTime / totalTime) * 100) + '%'
+  }
+
+  let onPause = () => {
+    ui.querySelector('.button').classList.add('button--play')
+    ui.querySelector('.button').classList.remove('button--pause')
+  }
+
+  let onPlay = () => {
+    ui.querySelector('.button').classList.remove('button--play')
+    ui.querySelector('.button').classList.add('button--pause')
+  }
+
+  // attach currentTime listener
+  player.addEventListener('timeupdate', onTimeUpdate)
+
+  // attach pause listener
+  player.addEventListener('pause', onPause)
+  player.addEventListener('play', onPlay)
+
+  // setup ui
+  ui.addEventListener('click', () => {
+    if (player.paused) {
+      player.play()
+    } else {
+      player.pause()
+    }
+  })
+}
+
+export default {
+  init
+}
--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import sidenotes from './sidenotes.js'
 import galleries from './galleries.js'
 import speak from './speak.js'
+import audioPlayer from './audio-player.js'
 
 function resize() {
   sidenotes.update()
@@ -11,5 +12,6 @@ window.addEventListener('resize', resize)
 galleries.init()
 sidenotes.init()
 speak.init()
+audioPlayer.init()
 resize()
 setTimeout(resize, 200)
