b2e95e260ad533f8b6b8ee88fbdd065f56c4aaf7
Eric Skogen <audionerd@users.noreply.github.com>
2020-08-17 16:32:43 -0500

    preserve title heading in list of speakable els

--- tools/markdown-to-speech/update-html-with-timestamps.js
+++ tools/markdown-to-speech/update-html-with-timestamps.js
@@ -4,18 +4,15 @@ const { JSDOM } = require('jsdom')
 const updateHtmlWithTimestamps = (htmlString, speakable) => {
   const dom = new JSDOM(htmlString)
 
-  let titleHeading = dom.window.document.querySelector('h1')
   for (let a of dom.window.document.querySelectorAll('a.header-anchor')) {
     let h = a.parentNode
 
-    if (h != titleHeading) {
-      let id = h.getAttribute('id')
-      let { position } = speakable.find(d => d.id == id)
+    let id = h.getAttribute('id')
+    let { position } = speakable.find(d => d.id == id)
 
-      let div = dom.window.document.createElement('div')
-      div.innerHTML = `<a class="speaker-icon" data-speak data-speak-start="${position}" href="#${id}"></a>`.trim()
-      h.prepend(div.firstChild)
-    }
+    let div = dom.window.document.createElement('div')
+    div.innerHTML = `<a class="speaker-icon" data-speak data-speak-start="${position}" href="#${id}"></a>`.trim()
+    h.prepend(div.firstChild)
   }
 
   for (let p of dom.window.document.querySelectorAll('p')) {
